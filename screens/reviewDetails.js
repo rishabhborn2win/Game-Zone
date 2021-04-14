05a24683245fea40881f9af549327f2667e73d11
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-04-14 10:38:25 +0530

    styling of homepage

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button, View, Text } from "react-native";
+import Card from '../shared/card';
 import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails({navigation}) {
 
     return (
         <View style={globalStyles.container}>
+            <Card>
             <Text>{ navigation.getParam('title') }</Text>
             <Text>{ navigation.getParam('body') }</Text>
             <Text>{ navigation.getParam('rating') }</Text> 
+            </Card>
         </View>
     )
 }
