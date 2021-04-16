c0354c499f8ce96f569345f76826d54c25d2f002
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-04-16 23:49:03 +0530

    Adding images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Button, View, Text } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
 import Card from '../shared/card';
-import { globalStyles } from '../styles/global';
+import { globalStyles, images } from '../styles/global';
 
 export default function ReviewDetails({navigation}) {
-
     return (
         <View style={globalStyles.container}>
             <Card>
             <Text>{ navigation.getParam('title') }</Text>
             <Text>{ navigation.getParam('body') }</Text>
-            <Text>{ navigation.getParam('rating') }</Text> 
+            <View style={styles.rating}>
+                <Text>GameZone rating: </Text>
+                <Image source={images.ratings[navigation.getParam('rating')]} />
+            </View>
+            {/* <Text>{ navigation.getParam('rating') }</Text>  */}
             </Card>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+
+    }
+})
